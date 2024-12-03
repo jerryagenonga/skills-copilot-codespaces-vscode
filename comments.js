e73a100479038a90c9aee7ef8158fee11887bf94
Jerry Agenonga <jerryagenonga@gmail.com>
2024-12-03 20:15:01 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,12 @@
+// Create web server
 // create a server
+
 const http = require('http');
 const url = require('url');
 const port = 3000;
-
 const requestHandler = (request, response) => {
   const parsedUrl = url.parse(request.url, true);
   response.setHeader('Content-Type', 'application/json');
-  
   if (parsedUrl.pathname === '/') {
     response.writeHead(200);
     response.end(JSON.stringify({ message: 'Hello, world!' }));
@@ -15,9 +15,7 @@ const requestHandler = (request, response) => {
     response.end(JSON.stringify({ message: 'Not Found' }));
   }
 };
-
 const server = http.createServer(requestHandler);
-
 server.listen(port, (err) => {
   if (err) {
     return console.log('Something went wrong', err);
